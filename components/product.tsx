a10692c755b2c2dd328316a22e21999f8923524a
ZafCode14 <zafarwp14@gmail.com>
2025-01-31 10:41:49 +0200

    product images

--- components/product.tsx
+++ components/product.tsx
@@ -1,6 +1,7 @@
 "use client";
 import calculatePrice from "@/lib/getPrice";
 import { Product as ProductType, Vendor } from "@/lib/types";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -16,7 +17,6 @@ function Product({ product, vendors }: Prop) {
   return (
     <Link href={p.startsWith('/insta') ? `/insta/product/${product.docID}` : `/shop/product/${product.docID}`} className="w-full h-full text-black">
       <div className={`w-full h-full object-cover overflow-hidden flex justify-center items-center bg-[white] rounded-md`}>
-        {/**
         <Image
           loading="lazy"
           src={product.images[0]}
@@ -25,7 +25,6 @@ function Product({ product, vendors }: Prop) {
           height={720}
           className={`object-contain h-full`}
         />
-         */}
       </div>
       <p className="font-bold leading-[16px] mt-[5px]">{product.name}</p>
       <p className="text-[12px]">{vendor?.name || 'Unknown Vendor'}</p> {/* Handle undefined vendor */}
