9ee0cdd7e5aa99696ddd85d314e1864b31213e76
ZafCode14 <zafarwp14@gmail.com>
2025-02-03 14:15:26 +0200

    image products

--- components/product.tsx
+++ components/product.tsx
@@ -17,8 +17,6 @@ function Product({ product, vendors }: Prop) {
   return (
     <Link href={p.startsWith('/insta') ? `/insta/product/${product.docID}` : `/shop/product/${product.docID}`} className="w-full h-full text-black">
       <div className={`w-full h-full object-cover overflow-hidden flex justify-center items-center bg-[white] rounded-md`}>
-        {/**
-         * 
         <Image
           loading="lazy"
           src={product.images[0]}
@@ -27,7 +25,6 @@ function Product({ product, vendors }: Prop) {
           height={1000}
           className={`object-contain h-full`}
         />
-         */}
       </div>
       <p className="font-bold leading-[16px] mt-[5px]">{product.name}</p>
       <p className="text-[12px]">{vendor?.name || 'Unknown Vendor'}</p> {/* Handle undefined vendor */}
